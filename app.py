38157ff1c795656818bd0c736d377e504f99172c
nidhish <nidhish1212@gmail.com>
2023-07-30 16:36:02 -0400

    updated model performance

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template, request
 import pickle
+from sklearn.preprocessing import Normalizer
+import pandas as pd
+import numpy as np
 
 
 app = Flask(__name__)
 
-# Replace this with your actual machine learning model
-def predict_fish(weight, length1, length2, length3, height, width):
+def predict_fish(data):
     # Load the Pickle model during app startup
     with open('FISH_CLASSIFICATION_MODEL_SVC.pkl', 'rb') as file:
         model = pickle.load(file)
-    prediction = model.predict([[weight, length1, length2, length3, height, width]])
 
-        # Given dictionary mapping fish species names to numeric labels
+    # Create a 2D array with a single row containing the feature values
+    features = data
+
+    # Make predictions using the loaded model
+    prediction = model.predict(features)
+
+    # Given dictionary mapping fish species names to numeric labels
     fish_species_dict = {
         0: "Bream",
         1: "Roach",
@@ -26,7 +33,6 @@ def predict_fish(weight, length1, length2, length3, height, width):
     predicted_label = prediction[0]
     predicted_species = fish_species_dict.get(predicted_label)
 
-    # Mocking the prediction by returning a fixed class label
     return predicted_species
 
 @app.route("/", methods=["GET"])
@@ -42,7 +48,25 @@ def predict():
     height = float(request.form["height"])
     width = float(request.form["width"])
 
-    predicted_fish = predict_fish(weight, length1, length2, length3, height, width)
+    data_dict = {
+    "Weight": [weight],
+    "Length1": [length1],
+    "Length2": [length2],
+    "Length3": [length3],
+    "Height": [height],
+    "Width": [width]
+    }
+
+    data = pd.DataFrame(data_dict)
+
+    norms= Normalizer().fit(data)
+    data_norms=norms.transform(data)
+    data_norms= np.asarray(data_norms)
+
+    nomred_data = pd.DataFrame(data_norms)
+
+
+    predicted_fish = predict_fish(nomred_data)
 
     return render_template("result.html", predicted_fish=predicted_fish)
 
